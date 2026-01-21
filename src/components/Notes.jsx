984b5ca669a372d4ab60c514de4ed31f46e73a83
frenzywho <amvermagaurav007@gmail.com>
2026-01-21 19:57:19 +0530

    add part 6-4

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -12,11 +12,18 @@ const Note = ({ note, handleClick }) => {
 
 const Notes = () => {
   const dispatch = useDispatch()
-  const notes = useSelector(state => state)
+  const notes = useSelector(({ filter, notes }) => {
+    if (filter === 'ALL') {
+      return notes
+    }
+    return filter === 'IMPORTANT'
+      ? notes.filter((note) => note.important)
+      : notes.filter((note) => !note.important)
+  })
 
   return (
     <ul>
-      {notes.map(note => (
+      {notes.map((note) => (
         <Note
           key={note.id}
           note={note}
